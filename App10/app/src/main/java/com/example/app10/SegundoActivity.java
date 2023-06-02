f5ca73b7f2aecf2fbca650057ac3f6cbd103ec96
mozart953 <wilsonnianopop@gmail.com>
2023-06-01 18:51:47 -0600

    App 10 modified

---
+++ App10/app/src/main/java/com/example/app10/SegundoActivity.java
@@ -0,0 +1,21 @@
+package com.example.app10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class SegundoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segundo);
+    }
+
+    public void Anterior(View view ){
+        Intent anterior = new Intent(this, MainActivity.class);
+        startActivity(anterior);
+    }
+}
