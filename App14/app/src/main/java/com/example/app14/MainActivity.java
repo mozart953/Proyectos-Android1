55fcdc974501bc1d6145f0c8c96b7afc9a338fda
mozart953 <wilsonnianopop@gmail.com>
2023-06-02 23:06:13 -0600

    App14

---
+++ App14/app/src/main/java/com/example/app14/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.app14;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText et_nombre, et_datos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et_nombre = (EditText) findViewById(R.id.txt_nombre);
+        et_datos = (EditText) findViewById(R.id.txt_2);
+    }
+
+    public void Guardar(View view){
+        String nombre = et_nombre.getText().toString();
+        String datos = et_datos.getText().toString();
+
+        SharedPreferences preferences = getSharedPreferences("agenda", Context.MODE_PRIVATE);
+        SharedPreferences.Editor obj_editor = preferences.edit();
+        obj_editor.putString(nombre, datos);
+        obj_editor.commit();
+        Toast.makeText(this, "El contacto ha sido guardado", Toast.LENGTH_LONG).show();
+
+    }
+}
