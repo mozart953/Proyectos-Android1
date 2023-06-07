03efd776643c7413e61b9aec7e4409ee430250dc
mozart953 <wilsonnianopop@gmail.com>
2023-06-06 19:22:54 -0600

    Query

--- App17/app/src/main/java/com/example/app17/MainActivity.java
+++ App17/app/src/main/java/com/example/app17/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.app17;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
@@ -49,6 +50,32 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this,"Se deben llenar todos los campos", Toast.LENGTH_SHORT).show();
         }
 
+
+    }
+
+    //Metodo para consultar
+
+    public void Buscar(View view){
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "administracion",null,1);
+        SQLiteDatabase BaseDeDataBase = admin.getWritableDatabase();
+
+        String codigo = et_codigo.getText().toString();
+
+        if(!codigo.isEmpty()){
+            Cursor fila  = BaseDeDataBase.rawQuery
+                    ("select descripcion, precio from articulos where codigo="+codigo, null);
+
+            if(fila.moveToFirst()){
+                et_descripcion.setText(fila.getString(0));
+                et_precio.setText(fila.getString(1));
+                BaseDeDataBase.close();
+            }else{
+                Toast.makeText(this, "No existe el articulo", Toast.LENGTH_SHORT).show();
+            }   BaseDeDataBase.close();
+
+        }else{
+            Toast.makeText(this, "Debes introducir el codigo del articulo", Toast.LENGTH_SHORT).show();
+        }
 
     }
 
