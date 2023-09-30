2c5c0db1982505399d7c0f2b60c375d8168c393c
mozart953 <wilsonnianopop@gmail.com>
2023-09-30 11:54:37 -0600

    Finishing example

--- RestConexion/app/src/main/java/com/example/restconexion/MainActivity.java
+++ RestConexion/app/src/main/java/com/example/restconexion/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.restconexion;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -17,6 +19,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.StringReader;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     EditText txtUser, txtTitle, txtBody;
@@ -35,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 LeedWs();
+                enviarWs(txtTitle.getText().toString(), txtBody.getText().toString(), txtUser.getText().toString());
             }
         });
     }
@@ -58,9 +63,47 @@ public class MainActivity extends AppCompatActivity {
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
+                Log.e("error", error.getMessage());
 
             }
         });
+
+        Volley.newRequestQueue(this, ).add(postRequest);
+    }
+
+    private void enviarWs(String title, String body, String userId){
+
+        String url = "https://jsonplaceholder.typicode.com/posts";
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    /*txtUser.setText(jsonObject.getString("userId"));
+                    String title = jsonObject.getString("title");
+                    txtTitle.setText(title);
+                    txtBody.setText(jsonObject.getString("body"));*/
+                    Toast.makeText(MainActivity.this, "RESULTADO ="+ response, Toast.LENGTH_LONG ).show();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("error", error.getMessage());
+
+            }
+        })
+        {
+            protected Map<String, String> getParams(){
+                Map<String, String> params = new HashMap<>();
+                params.put("title", title);
+                params.put("body", body);
+                params.put("userId", userId);
+                return params;
+            }
+        };
 
         Volley.newRequestQueue(this, ).add(postRequest);
     }
